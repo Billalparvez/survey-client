56eed6d865ef10447c0ad9a00a2fabfb90e160f7
billalparvez <billalparvez80@gmail.com>
2023-12-03 18:59:55 +0600

    readme file add

--- src/Pages/Home/FeaturedSurvey/Card.jsx
+++ src/Pages/Home/FeaturedSurvey/Card.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import Tilt from 'react-parallax-tilt';
 import ReactParallaxTilt from "react-parallax-tilt";
 import { Link } from "react-router-dom";
--- src/Pages/Home/LatestSurvey/Card.jsx
+++ src/Pages/Home/LatestSurvey/Card.jsx
@@ -6,15 +6,13 @@ const Card = ({ survey }) => {
     console.log(survey)
     return (
         <Tilt>
-            <div className="card relative card-compact mb-2 bg-base-100 shadow-xl">
-                <div data-aos="flip-up" className="card   bg-base-100  hover:border-b-2 border-info ">
+            <div className="card  card-compact mb-2 bg-base-100 shadow-xl h-[450px]">
+                <div data-aos="flip-up" className="  bg-base-100  ">
                     <div className="">
                         <figure><img className=" object-cover h-64 rounded-xl hover:hue-rotate-90 w-full" src={image} alt="Shoes" /></figure>
                     </div>
-                    <div className="card-body">
-                        <div className="">
-                            <img className="absolute bottom-[140px] md:bottom-[147px] w-14 h-14 rounded-full border-4 border-white " src={image} alt="" />
-                        </div>
+                    <div className="card-body relative">
+                        <img className="sb absolute  mt-[-44px]  w-14 h-14 rounded-full border-4 border-white " src={image} alt="" />
                         <p className="font-bold mt-3 text-gray-400 md:text-lg border-l-4 border-info "><span className="ml-3">{title}</span></p>
                         <h2 className="card-title ">
                             {description}
